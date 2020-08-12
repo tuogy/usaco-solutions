c42e48a204e616400bdaaa83c00beea132cd8d1f
Bichuan Guo <guobichuan@gmail.com>
2020-08-13 05:32:34 +0800

    milk6: parallel edges and minimum number of cuts

--- milk6.cc
+++ milk6.cc
@@ -18,12 +18,12 @@ using ll = long long;
 int V, E, s, t;
 vector<int> adj[32];
 
-ll cap[32][32];
-int cap_orig[32][32];
-ll rG[32][32];
-int edge_id[32][32];
+vector<int> cap[32][32];
+vector<ll> rG[32][32];
+vector<int> edge_id[32][32];
 bool vis[32];
 int parent[32];
+int parent_edge[32];
 
 bool bfs() {
     memset(vis, 0, V * sizeof(bool));
@@ -32,8 +32,14 @@ bool bfs() {
     while (!q.empty() && !vis[t]) {
         int u = q.front(); q.pop();
         for (auto v : adj[u]) {
-            if (!vis[v] && rG[u][v] > 0) 
-                vis[v] = true, q.push(v), parent[v] = u;
+            if (vis[v]) continue;
+            for (int i = 0; i < rG[u][v].size(); i++) {
+                if (rG[u][v][i] > 0) {
+                    q.push(v), vis[v] = true;
+                    parent[v] = u, parent_edge[v] = i;
+                    break;
+                }
+            }
         }
     }
     return vis[t];
@@ -45,41 +51,61 @@ int main() {
     
     fin >> V >> E;
     s = 0, t = V - 1;
-    for (int i = 0; i < V; i++) adj[i].clear();
+    for (int i = 0; i < V; i++) {
+        adj[i].clear();
+        for (int j = 0; j < V; j++) {
+            cap[i][j].clear();
+            rG[i][j].clear();
+            edge_id[i][j].clear();
+        }
+    }
+
+    vector<vector<bool>> adj_mat(V, vector<bool>(V, false));
     for (int i = 0; i < E; i++) {
-        int u, v; ll w;
+        int u, v, w;
         fin >> u >> v >> w;
         u--, v--;
-        adj[u].push_back(v), adj[v].push_back(u);
-        cap_orig[u][v] += w;
-        cap[u][v] += w * E + 1;
-        edge_id[u][v] = i;
+        adj_mat[u][v] = adj_mat[v][u] = true;
+        cap[u][v].push_back(w), cap[v][u].push_back(0);
+        rG[u][v].push_back(w * E + 1LL), rG[v][u].push_back(0);
+        edge_id[u][v].push_back(i), edge_id[v][u].push_back(i);
     }
     // modify capacity to generate valid solutions
     // parallel edges
 
-    memcpy(rG, cap, V * 32 * sizeof(ll));
+    for (int i = 0; i < V; i++) {
+        for (int j = i; j < V; j++) {
+            if (adj_mat[i][j]) {
+                adj[i].push_back(j);
+                adj[j].push_back(i);
+            }
+        }
+    }
+
     while (bfs()) {
         ll f = LLONG_MAX;
         for (int v = t; v != s; v = parent[v])
-            f = min(f, rG[parent[v]][v]);
+            f = min(f, rG[parent[v]][v][parent_edge[v]]);
         for (int v = t; v != s; v = parent[v])
-            rG[parent[v]][v] -= f, rG[v][parent[v]] += f;
+            rG[parent[v]][v][parent_edge[v]] -= f, rG[v][parent[v]][parent_edge[v]] += f;
     }
 
     vector<int> ret;
     int min_cut = 0;
     for (int u = 0; u < V; u++) {
+        if (!vis[u]) continue;
         for (int v = 0; v < V; v++) {
-            if (vis[u] && !vis[v] && cap_orig[u][v] > 0) {
-                min_cut += cap_orig[u][v];
-                ret.push_back(edge_id[u][v] + 1);
+            if (vis[v]) continue;
+            for (int i = 0; i < rG[u][v].size(); i++) {
+                if (cap[u][v][i] > 0 && rG[u][v][i] == 0)
+                    min_cut += cap[u][v][i], ret.push_back(edge_id[u][v][i] + 1);
             }
         }
     }
+    
     fout << min_cut << " " << ret.size() << endl;
     sort(ret.begin(), ret.end());
-    for (int i = 0; i < ret.size(); i++) fout << ret[i] << (i + 1 == ret.size() ? '\n' : ' ');
+    for (int i = 0; i < ret.size(); i++) fout << ret[i] << endl;
 
     return 0;
 }
